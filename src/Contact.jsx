6ca8529f12bc4286bd44b87c6a3aa874556396ad
Syed Furquan Ahmad <furquan@pm.me>
2022-03-13 11:46:56 +0530

    Added placeholder Contact page

---
+++ src/Contact.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Contact extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>Copyright &#169; 2022 Syed Furquan Ahmad</h2>
+                <h2>All rights reserved.</h2>
+                <main>
+                    <p>This section contains contact information...</p>
+                </main>
+            </div>
+        )
+    }
+}
+
+export default Contact;
