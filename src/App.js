545cd3a177cffcc5d84619da608452410ba747d5
Syed Furquan Ahmad <furquan@pm.me>
2022-03-14 14:21:38 +0530

    Added Header and Footer components, moved header elements from Home into Header

--- src/App.js
+++ src/App.js
@@ -5,31 +5,46 @@ import About from './About';
 import Home from './Home';
 import Contact from './Contact';
 
+const Footer = () => {
+  return (
+    <div style={{color: 'white'}}>
+      Copyright (C) 2022 Syed Furquan Ahmad. All rights reserved.
+    </div>
+  )
+}
+
+const Header = () => {
+  return(
+    <div style={{background: 'white'}}>
+      <nav>
+          <ul id="navigation">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+    </div>
+  )
+}
+
 class App extends React.Component {
   render() {
-      return (
-        <div className="App">
-          <div>
-            <nav>
-              <ul id="navigation">
-                <li>
-                  <Link to="/">Home</Link>
-                </li>
-                <li>
-                <Link to="/about">About</Link>
-                </li>
-                <li>
-                <Link to="/contact">Contact</Link>
-                </li>
-              </ul>
-            </nav>
-          </div>
-          <Routes>
-            <Route path='/' element={<Home/>} />
-            <Route path="/about" element={<About/>} />
-            <Route path="/contact" element={<Contact/>} />
-          </Routes>
-          </div>
+    return (
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        <Footer />
+      </div>
     );
   }
 }
