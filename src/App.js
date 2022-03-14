0b867e16cf73f9926bb17726c27f74501fda3847
Syed Furquan Ahmad <furquan@pm.me>
2022-03-14 14:30:56 +0530

    Code refactoring: moved to functional components

--- src/App.js
+++ src/App.js
@@ -7,46 +7,44 @@ import Contact from './Contact';
 
 const Footer = () => {
   return (
-    <div style={{color: 'white'}}>
+    <div style={{ color: 'white' }}>
       Copyright (C) 2022 Syed Furquan Ahmad. All rights reserved.
     </div>
-  )
+  );
 }
 
 const Header = () => {
-  return(
-    <div style={{background: 'white'}}>
+  return (
+    <div style={{ background: 'white' }}>
       <nav>
-          <ul id="navigation">
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/contact">Contact</Link>
-            </li>
-          </ul>
-        </nav>
+        <ul id="navigation">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
     </div>
-  )
+  );
 }
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <Header />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-        <Footer />
-      </div>
-    );
-  }
+const App = () => {
+  return (
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
