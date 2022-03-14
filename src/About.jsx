8aa43a211c6944ea3cc688bab6c4e89e45231f39
Syed Furquan Ahmad <furquan@pm.me>
2022-03-14 14:18:46 +0530

    Code refactoring: Move header content to App, add divStyle to About and fixed formatting.

--- src/About.jsx
+++ src/About.jsx
@@ -6,16 +6,16 @@ const divStyle = {
 };
 
 class About extends React.Component {
-    render() {
-        return (
-            <div style={divStyle}>
-                <h2>About Page</h2>
-                <main>
-                    <p>This section contains information about...</p>
-                </main>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div style={divStyle}>
+        <h2>About Page</h2>
+        <main>
+          <p>This section contains information about...</p>
+        </main>
+      </div>
+    )
+  }
 }
 
 export default About;
--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
 
+const divStyle = {
+  color:'white',
+  background:'gray'
+};
+
 class Contact extends React.Component {
-    render() {
-        return (
-            <div>
-                <h2>Copyright &#169; 2022 Syed Furquan Ahmad</h2>
-                <h2>All rights reserved.</h2>
-                <main>
-                    <p>This section contains contact information...</p>
-                </main>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div style={divStyle}>
+        <h2>Copyright &#169; 2022 Syed Furquan Ahmad</h2>
+        <h2>All rights reserved.</h2>
+        <main>
+        <p>This section contains contact information...</p>
+        </main>
+      </div>
+    )
+  }
 }
 
 export default Contact;
