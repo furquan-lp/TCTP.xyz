2a8c641adbe6b485a1280810be2bd491a0a74f7b
Syed Furquan Ahmad <furquan@pm.me>
2022-03-16 20:44:09 +0530

    Moved styles to Components.css

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,14 +1,10 @@
+import './Components.css';
 import React from 'react';
-
-const divStyle = {
-  color: 'white',
-  background: 'gray'
-};
 
 const About = () => {
   document.title = 'About | The Chip Tinker Project';
   return (
-    <div style={divStyle}>
+    <div className="About">
       <h2>About Page</h2>
       <main>
         <p>This section contains information about...</p>
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,14 +1,10 @@
+import './Components.css';
 import React from 'react';
-
-const divStyle = {
-  color: 'white',
-  background: 'gray'
-};
 
 const Contact = () => {
   document.title = 'Contact | The Chip Tinker Project';
   return (
-    <div style={divStyle}>
+    <div className="Contact">
       <h2>Copyright &#169; 2022 Syed Furquan Ahmad</h2>
       <h2>All rights reserved.</h2>
       <main>
