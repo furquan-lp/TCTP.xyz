23fe8bfdf2ce485366d7f10a03a538d2ebd0c175
Syed Furquan Ahmad <furquan@pm.me>
2022-05-05 21:30:22 +0530

    Added welcome text, blog blurb and a relevant image to the Welcome component

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -4,7 +4,15 @@ import '../style.css';
 import arduinoVector from '../arduino_img_1-.jpeg';
 
 const Welcome = () => <div className="welcome">
-  Welcome!
+  <span id="big-text">
+    <u>Welcome!</u>
+    <span id="small-text">
+      The Chip Tinker Project is a blog about exploring tech and the internet
+      with complete tinkering freedom.<br />
+      <a href="https://tctp.xyz/" id="readmore">Learn more...</a>
+    </span>
+  </span>
+  <img src={arduinoVector} alt="assortment of electronic components" id="welcomeimg" />
 </div>;
 
 export default Welcome;
