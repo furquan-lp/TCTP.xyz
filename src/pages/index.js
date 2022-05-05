864f4eb3e960e3d070e81d5f69ede9e579357720
Syed Furquan Ahmad <furquan@pm.me>
2022-05-05 21:31:32 +0530

    Added Welcome component to Home

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import Header from '../components/header';
+import Welcome from '../components/welcome';
 import '../style.css';
 
 export default function Home() {
   return (
     <div className="Home">
       <Header />
+      <Welcome />
     </div>
   );
 };
