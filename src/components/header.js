e1631dbd306385cbbac38ffbc14681dead851ae4
Syed Furquan Ahmad <furquan@pm.me>
2022-05-05 19:19:41 +0530

    Added Header component with a top bar containing the TCTP logo and links

---
+++ src/components/header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import '../style.css';
+import sitelogo from '../logo.svg';
+
+const Header = () => <div className="header">
+  <div className="tctp-logo">
+    <img src={sitelogo} alt="The Chip Tinker Project" />
+    <a href='https://tctp.xyz'>
+      TCTP - The Chip Tinker Project
+    </a>
+  </div>
+  <div className="header-links">
+    <a href='https://tctp.xyz/'>Blog</a>
+    <a href='https://tctp.xyz/'>About</a>
+  </div>
+</div>;
+
+export default Header;
