431eed03419e4c5872ee6dc9d959ab7e4d5c8989
Syed Furquan Ahmad <furquan@pm.me>
2022-05-05 19:24:35 +0530

    Added Welcome component to display welcome message and the relevant image

---
+++ src/components/welcome.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import '../style.css';
+import arduinoVector from '../arduino_img_1-.jpeg';
+
+const Welcome = () => <div className="welcome">
+  Welcome!
+</div>;
+
+export default Welcome;
