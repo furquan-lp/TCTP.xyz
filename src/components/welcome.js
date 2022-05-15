25bd0a4578e6773c07b2dcced65d686dc2a61a72
Syed Furquan Ahmad <furquan@pm.me>
2022-05-15 19:14:29 +0530

    Use description prop instead of hardcoded text

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -3,12 +3,10 @@ import React from 'react';
 import '../style.css';
 import arduinoVector from '../arduino_img_1-.jpeg';
 
-const Welcome = () => <div className="welcome">
+const Welcome = ({ description }) => <div className="welcome">
   <span id="big-text">
     <u>Welcome!</u>
-    <span id="small-text">
-      The Chip Tinker Project is a blog about exploring tech and the internet
-      with complete tinkering freedom.<br />
+    <span id="small-text">{description}<br />
       <a href="https://tctp.xyz/" id="readmore">Learn more...</a>
     </span>
   </span>
