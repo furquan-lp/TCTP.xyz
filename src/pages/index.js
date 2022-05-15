7e041fb1c416ca10d60acc1afc7483650cde5ec3
Syed Furquan Ahmad <furquan@pm.me>
2022-05-15 18:56:07 +0530

    Convert Home to arrow function

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,11 +4,10 @@ import Header from '../components/header';
 import Welcome from '../components/welcome';
 import '../style.css';
 
-export default function Home() {
-  return (
-    <div className="Home">
-      <Header />
-      <Welcome />
-    </div>
-  );
-};
+const Home = () =>
+  <div className="Home">
+    <Header />
+    <Welcome />
+  </div>;
+
+export default Home;
