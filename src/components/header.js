32cd05564902cc91c4e8d7f38c22a900e96bcfe9
Syed Furquan Ahmad <furquan@pm.me>
2022-05-17 20:08:21 +0530

    Rearranged tags to make both the logo text and the logo image links to the homepage, added a filter effect upon hover on the logo image using id selectors (additionally, used id selectors where approrpiate in general)

--- src/components/header.js
+++ src/components/header.js
@@ -3,17 +3,16 @@ import React from 'react';
 import './header.css';
 import sitelogo from '../logo.svg';
 
-const Header = () => <div className="header">
-  <div className="tctp-logo">
-    <img src={sitelogo} alt="The Chip Tinker Project" />
-    <a href='https://tctp.xyz'>
-      TCTP - The Chip Tinker Project
+const Header = () =>
+  <div className="header">
+    <a href='https://tctp.xyz' id="logo-link">
+      <img src={sitelogo} alt="The Chip Tinker Project" id="logo-filter" />
+      <span id="logo-title">TCTP - The Chip Tinker Project</span>
     </a>
-  </div>
-  <div className="header-links">
-    <a href='https://tctp.xyz/'>Blog</a>
-    <a href='https://tctp.xyz/'>About</a>
-  </div>
-</div>;
+    <div className="header-links">
+      <a href='https://tctp.xyz/'>Blog</a>
+      <a href='https://tctp.xyz/'>About</a>
+    </div>
+  </div>;
 
 export default Header;
