099547725c5d4bf66d1151c955f5f9bbcf58d1c2
Syed Furquan Ahmad <furquan@pm.me>
2022-05-19 00:03:32 +0530

    Use image passed from props instead of importing the welcome image from a hardcoded path

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -1,16 +1,15 @@
 import React from 'react';
 
 import '../style.css';
-import arduinoVector from '../images/arduino_img_1-.jpeg';
 
-const Welcome = ({ description }) => <div className="welcome">
+const Welcome = ({ description, image }) => <div className="welcome">
   <span id="big-text">
     <u>Welcome!</u>
     <span id="small-text">{description}<br />
       <a href="https://tctp.xyz/" id="readmore">Learn more...</a>
     </span>
   </span>
-  <img src={arduinoVector} alt="assortment of electronic components" id="welcomeimg" />
+  <img src={image} alt="assortment of electronic components" id="welcomeimg" />
 </div>;
 
 export default Welcome;
