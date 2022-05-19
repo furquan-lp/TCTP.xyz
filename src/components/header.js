c364e8849012d00b22aa9c0a04f3cf9d830da0ac
Syed Furquan Ahmad <furquan@pm.me>
2022-05-19 16:36:27 +0530

    Use gatsby Link to the relevant pages (About and Blog) instead of placeholder hyperlinks

--- src/components/header.js
+++ src/components/header.js
@@ -1,17 +1,18 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
 import './header.css';
 import sitelogo from '../logo.svg';
 
 const Header = () =>
   <div className="header">
-    <a href='https://tctp.xyz' id="logo-link">
+    <Link to="/" id="logo-link">
       <img src={sitelogo} alt="The Chip Tinker Project" id="logo-filter" />
       <span id="logo-title">TCTP - The Chip Tinker Project</span>
-    </a>
+    </Link>
     <div className="header-links">
-      <a href='https://tctp.xyz/'>Blog</a>
-      <a href='https://tctp.xyz/'>About</a>
+      <Link to="/blog" className="header-link">Blog</Link>
+      <Link to="/about" className="header-link">About</Link>
     </div>
   </div>;
 
