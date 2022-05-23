34efb2062afb8a4b537107f5c2f108ddf1bd36d5
Syed Furquan Ahmad <furquan@pm.me>
2022-05-23 19:34:11 +0530

    Added the Footer component with copyright notice and source code link.

---
+++ src/components/footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './footer.css';
+
+const Footer = () =>
+  <div className="footer">
+    <span id="copyright-text">
+      Copyright &copy; 2022 Syed Furquan Ahmad. All rights reserved.
+    </span>
+    <a href="https://github.com/furquan-lp/tctp.xyz" id="footer-link">
+      View source code on GitHub.
+    </a>
+  </div>;
+
+export default Footer;
