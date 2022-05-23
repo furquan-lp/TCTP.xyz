64aeb47059b050693a61ad9487550043cc83c5ed
Syed Furquan Ahmad <furquan@pm.me>
2022-05-23 19:49:49 +0530

    Added IconContext.Provider to FiGithub icon to control size and add className styling

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FiGithub } from '@react-icons/all-files/fi/FiGithub';
+import { IconContext } from '@react-icons/all-files';
 
 import './footer.css';
 
@@ -9,8 +10,12 @@ const Footer = () =>
       Copyright &copy; 2022 Syed Furquan Ahmad. All rights reserved.
     </span>
     <a href="https://github.com/furquan-lp/tctp.xyz" id="footer-link">
-      <FiGithub />
       View source code on GitHub
+      <IconContext.Provider value={
+        { size: '1rem', className: 'github-icon' }
+      }>
+        <FiGithub />
+      </IconContext.Provider>
     </a>
   </div>;
 
