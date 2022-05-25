c81662d60de107eb24db187f091f064378ec0243
Syed Furquan Ahmad <furquan@pm.me>
2022-05-25 22:42:54 +0530

    Use separate className for big-card article title

--- src/components/stories.js
+++ src/components/stories.js
@@ -12,7 +12,7 @@ const BigCard = ({ image, imageAlt, post }) =>
     <img src={image} alt={imageAlt} className="card-img" />
     <div className="card-article">
       <div className="card-article-content">
-        <Link to={'blog' + post.fields.slug} className="card-article-title">
+        <Link to={'blog' + post.fields.slug} className="big-card-article-title">
           {post.frontmatter.title}
         </Link>
         <div className="card-article-date">
