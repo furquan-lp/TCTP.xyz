a954b60268fac2b21e19e968dbc47f2f3dca5b06
Syed Furquan Ahmad <furquan@pm.me>
2022-05-26 09:01:34 +0530

    Added thumbnail to card using GatsbyImage from gatsby-plugin-image

--- src/components/cards.js
+++ src/components/cards.js
@@ -6,10 +6,10 @@ import './cards.css';
 
 const Card = ({ post }) =>
   <div className="card">
+    <GatsbyImage
+      image={post.frontmatter.thumbnail.childImageSharp.gatsbyImageData}
+    />
     <article className="card-article">
-      <GatsbyImage
-        image={post.frontmatter.thumbnail.childImageSharp.gatsbyImageData}
-      />
       <Link to={'.' + post.fields.slug} className="card-article-title">
         {post.frontmatter.title}
       </Link>
