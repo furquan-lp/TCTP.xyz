03cb6d7dd8ac7b042ec90d82f4679c9c4118d406
Syed Furquan Ahmad <furquan@pm.me>
2022-05-26 20:10:23 +0530

    Added Header and Footer components

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 
+import Header from '../components/header';
+import Footer from '../components/footer';
+
 import '../style.css';
 
 export default function BlogPost({ data }) {
   const post = data.markdownRemark;
   return (
     <div className="blog">
+      <Header />
       <h1>{post.frontmatter.title}</h1>
       <small>{post.frontmatter.date}</small>
       <div dangerouslySetInnerHTML={{ __html: post.html }} />
+      <Footer />
     </div>
   );
 };
