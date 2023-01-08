07efc3ceba9723521b535894fa7725d6b05cc906
Syed F Ahmad <furquan@protonmail.com>
2023-01-09 00:42:17 +0530

    Replaced 'All rights reserved' text with build version imported from package.json

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { FiGithub } from '@react-icons/all-files/fi/FiGithub';
 import { IconContext } from '@react-icons/all-files';
 
+import { version } from '../../package.json';
 import './footer.css';
 
 const Footer = () =>
   <div className="footer">
     <span id="copyright-text">
-      Copyright &copy; 2022 Syed Furquan Ahmad. All rights reserved.
+      Copyright &copy; 2022 Syed Furquan Ahmad. Build version {version}.
     </span>
     <a href="https://github.com/furquan-lp/tctp.xyz" id="footer-link">
       View source code on GitHub
