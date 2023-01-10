7431c2f9aca53db1645db271af97ad7521fecacb
Syed F Ahmad <furquan@protonmail.com>
2023-01-10 20:42:59 +0530

    Converted logo-link and logo-filter to Tailwind classes with responsive sizes

--- src/components/header.js
+++ src/components/header.js
@@ -6,8 +6,9 @@ import sitelogo from '../../assets/logo.svg';
 
 const Header = () =>
   <div className="flex items-center justify-between shadow-panel pb-0.6">
-    <Link to="/" id="logo-link">
-      <img src={sitelogo} alt="The Chip Tinker Project" id="logo-filter" />
+    <Link to="/" className="flex items-center">
+      <img src={sitelogo} alt="The Chip Tinker Project" className="ml-2 w-10 h-10 md:w-12 md:h-12 transition-all
+      duration-300 hover:brightness-125 hover:contrast-125" />
       <span id="logo-title">TCTP - The Chip Tinker Project</span>
     </Link>
     <div className="header-links">
