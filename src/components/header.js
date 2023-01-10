4063df58019751ad462e60d5d9f23540b3002371
Syed F Ahmad <furquan@protonmail.com>
2023-01-10 20:26:30 +0530

    Use 'panel' box shadow from theme and reduce padding on main container

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import './header.css';
 import sitelogo from '../../assets/logo.svg';
 
 const Header = () =>
-  <div className="flex items-center justify-between shadow-sm pb-1">
+  <div className="flex items-center justify-between shadow-panel pb-0.6">
     <Link to="/" id="logo-link">
       <img src={sitelogo} alt="The Chip Tinker Project" id="logo-filter" />
       <span id="logo-title">TCTP - The Chip Tinker Project</span>
