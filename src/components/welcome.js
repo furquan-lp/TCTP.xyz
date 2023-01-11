9b895276230715e9b81dae95cb018b1451569511
Syed F Ahmad <furquan@protonmail.com>
2023-01-11 17:19:43 +0530

    Added responsive styling for smaller screens by enabling flex wrap, reducing welcomeimg width and adjusting margins

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'gatsby';
 
 const Welcome = ({ description, image }) =>
-  <div className="flex justify-between mt-12">
+  <div className="flex justify-between mt-4 md:mt-12 flex-wrap md:flex-nowrap">
     <div className="flex flex-col text-blue-deep">
       <span className="underline text-3xl md:text-6xl tracking-wider">Welcome!</span>
       <span className="tracking-wide mt-2 text-lg md:text-2xl">{description}<br />
@@ -11,8 +11,8 @@ const Welcome = ({ description, image }) =>
           hover:bg-green-light hover:text-blue-dark transition-all duration-200">Learn more...</Link>
       </span>
     </div>
-    <Link to="blog/what-is-tctp">
-      <img src={image} alt="assortment of electronic components" className="rounded-md opacity-70 w-full h-full
+    <Link to="blog/what-is-tctp" className="my-1 md:my-0">
+      <img src={image} alt="assortment of electronic components" className="rounded-md opacity-70 w-4/5 md:w-full h-full
       shadow-md hover:opacity-90 transition-all duration-200" />
     </Link>
   </div>;
