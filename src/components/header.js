7d2c9171b9e317b7bb3067f1b6e0de0fdf0db55f
Syed F Ahmad <furquan@protonmail.com>
2023-01-11 17:35:51 +0530

    Added backdrop-blur to main container to make the header stand out for smaller screens

--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,7 @@ import { Link } from 'gatsby';
 import sitelogo from '../../assets/logo.svg';
 
 const Header = () =>
-  <div className="flex items-center justify-between shadow-panel pb-2">
+  <div className="flex items-center justify-between shadow-panel pb-2 backdrop-blur">
     <Link to="/" className="flex items-center">
       <img src={sitelogo} alt="The Chip Tinker Project" className="ml-2 w-8 h-8 md:w-12 md:h-12 transition-all
       duration-300 hover:brightness-125 hover:contrast-125" />
