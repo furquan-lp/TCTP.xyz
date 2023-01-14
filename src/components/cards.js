d7534398c6a5285d7e1329c0fd025f46d4ea14ad
Syed F Ahmad <furquan@protonmail.com>
2023-01-15 00:38:18 +0530

    Fixed the cards styling, added some shadow, padding and made the background white (for smaller screens)

--- src/components/cards.js
+++ src/components/cards.js
@@ -6,18 +6,18 @@ import { FiCalendar } from '@react-icons/all-files/fi/FiCalendar';
 import './cards.css';
 
 const Card = ({ post }) =>
-  <div className="flex flex-col w-11/12 md:w-5/12 mt-4 mr-2 md:mr-4 rounded">
+  <div className="flex flex-col w-11/12 md:w-5/12 mt-4 md:mr-4 rounded-md shadow-lg p-2 bg-white">
     <GatsbyImage
       image={post.frontmatter.thumbnail.childImageSharp.gatsbyImageData}
       className="rounded-lg shadow-lg mb-2 md:mb-4"
     />
-    <article className="flex flex-col justify-around">
+    <article className="flex flex-col justify-between">
       <Link to={'.' + post.fields.slug} className="tracking-wide text-2xl md:text-3xl font-bold">
         {post.frontmatter.title}
       </Link>
       <section className="flex justify-between mt-0.5 items-center text-blue-dark">
         {post.frontmatter.author}
-        <span><FiCalendar /> {post.frontmatter.date}</span>
+        <span><FiCalendar />{post.frontmatter.date}</span>
       </section>
       <p>{post.excerpt}</p>
       <section className="my-4">
