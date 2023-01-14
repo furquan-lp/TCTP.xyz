7380972f28f076187114f3e96844f9453d49263b
Syed F Ahmad <furquan@protonmail.com>
2023-01-14 21:41:40 +0530

    Convert the CSS classes to Tailwind classes

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,14 +6,14 @@ import { version } from '../../package.json';
 import './footer.css';
 
 const Footer = () =>
-  <div className="footer">
-    <span id="copyright-text">
+  <div className="flex items-center justify-between border-t border-slate-300 mt-8 mb-2 pt-1 text-sm text-slate-500">
+    <span>
       Copyright &copy; {new Date().getFullYear()} Syed Furquan Ahmad. Build version {version}.
     </span>
-    <a href="https://github.com/furquan-lp/tctp.xyz" id="footer-link">
+    <a href="https://github.com/furquan-lp/tctp.xyz" className="flex hover:underline">
       View source code on GitHub
       <IconContext.Provider value={
-        { size: '1rem', className: 'github-icon' }
+        { size: '1rem', className: 'self-center ml-1' }
       }>
         <FiGithub />
       </IconContext.Provider>
