8cc0e2fa983972af0a5c6840d6d7dfb090648073
Syed F Ahmad <furquan@protonmail.com>
2023-01-14 21:44:18 +0530

    No need to import footer.css anymore

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,6 @@ import { FiGithub } from '@react-icons/all-files/fi/FiGithub';
 import { IconContext } from '@react-icons/all-files';
 
 import { version } from '../../package.json';
-import './footer.css';
 
 const Footer = () =>
   <div className="flex items-center justify-between border-t border-slate-300 mt-8 mb-2 pt-1 text-sm text-slate-500">
