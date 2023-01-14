bd4c80eba6615dc9624416536b9f97aad20f13fb
Syed F Ahmad <furquan@protonmail.com>
2023-01-15 00:45:20 +0530

    Made the 'TCTP - ' text hidden on screens smaller than md so it does not wrap to the next line

--- src/components/header.js
+++ src/components/header.js
@@ -6,10 +6,13 @@ import sitelogo from '../../assets/logo.svg';
 const Header = () =>
   <div className="flex items-center justify-between shadow-panel pb-2 backdrop-blur">
     <Link to="/" className="flex items-center">
-      <img src={sitelogo} alt="The Chip Tinker Project" className="ml-2 w-8 h-8 md:w-12 md:h-12 transition-all
+      <img src={sitelogo} alt="The Chip Tinker Project" className="m-1 md:ml-2 w-8 h-8 md:w-12 md:h-12 transition-all
       duration-300 hover:brightness-125 hover:contrast-125" />
-      <span className="font-logofont text-3xl md:text-6xl ml-2 text-green-dark select-none transition-all duration-300
-      md:hover:text-bigger-logo-title hover:text-green-light">TCTP - The Chip Tinker Project</span>
+      <span className="font-logofont text-2xl md:text-6xl ml-2 text-green-dark select-none transition-all duration-300
+      md:hover:text-bigger-logo-title hover:text-green-light">
+        <span className="hidden md:inline">TCTP - </span>
+        The Chip Tinker Project
+      </span>
     </Link>
     <div>
       <Link to="/blog" className="mr-2 text-lg md:text-2xl text-green-dark tracking-wider underline
