bfdcbbe287a219b21db425af1968be2bd33e1a6d
Syed F Ahmad <furquan@protonmail.com>
2023-01-15 00:18:23 +0530

    Converted all CSS classes to Tailwind classes with responsive styling, also fixed styling and margins

--- src/components/cards.js
+++ src/components/cards.js
@@ -6,22 +6,23 @@ import { FiCalendar } from '@react-icons/all-files/fi/FiCalendar';
 import './cards.css';
 
 const Card = ({ post }) =>
-  <div className="card">
+  <div className="flex flex-col w-11/12 md:w-5/12 mt-4 mr-2 md:mr-4 rounded">
     <GatsbyImage
       image={post.frontmatter.thumbnail.childImageSharp.gatsbyImageData}
-      className="card-img"
+      className="rounded-lg shadow-lg mb-2 md:mb-4"
     />
-    <article className="card-article">
-      <Link to={'.' + post.fields.slug} className="card-article-title-link">
+    <article className="flex flex-col justify-around">
+      <Link to={'.' + post.fields.slug} className="tracking-wide text-2xl md:text-3xl font-bold">
         {post.frontmatter.title}
       </Link>
-      <section className="card-article-subtitle">
+      <section className="flex justify-between mt-0.5 items-center text-blue-dark">
         {post.frontmatter.author}
         <span><FiCalendar /> {post.frontmatter.date}</span>
       </section>
       <p>{post.excerpt}</p>
-      <section className="card-article-button">
-        <Link to={'.' + post.fields.slug} className="card-article-link">
+      <section className="my-4">
+        <Link to={'.' + post.fields.slug} className="rounded-md text-light-new bg-blue-dark p-2
+         shadow underline md:no-underline md:hover:underline hover:bg-blue-deep transition-all duration-200">
           READ ARTICLE
         </Link>
       </section>
