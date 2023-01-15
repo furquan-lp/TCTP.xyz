a998ab68939c1550029677a05dbfc89ad6f6692b
Syed F Ahmad <furquan@protonmail.com>
2023-01-15 19:44:52 +0530

    Added some styling to attribution links by moving them into a container

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,13 +4,13 @@ import { IconContext } from '@react-icons/all-files';
 
 import { version } from '../../package.json';
 
-const Footer = ({ attriblink, attrib }) =>
+const Footer = ({ attribs }) =>
   <div className="flex items-center justify-between border-t border-slate-300 mt-8 mb-2 pt-1 text-sm text-slate-500">
     <span>
       Copyright &copy; {new Date().getFullYear()} Syed Furquan Ahmad.
       <span className="hidden md:inline"> Build version {version}.</span>
     </span>
-    <a href={attriblink}>{attrib}</a>
+    <div className="flex flex-col">{attribs.map(a => <a href={a.attriblink}>{a.attrib}</a>)}</div>
     <a href="https://github.com/furquan-lp/tctp.xyz" className="flex hover:underline">
       <span className="hidden md:inline">View source code on GitHub</span>
       <IconContext.Provider value={
