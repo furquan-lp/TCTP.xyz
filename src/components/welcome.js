c69268ffb7b424864fc220f23cf1a1be9eacbf31
Syed F Ahmad <furquan@protonmail.com>
2023-06-23 11:48:37 +0530

    Changed links from what-is-tctp to /about

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -6,12 +6,12 @@ const Welcome = ({ description, image }) =>
     <div className="flex flex-col">
       <span className="md:underline text-4xl md:text-6xl tracking-wider">Welcome!</span>
       <span className="tracking-wide mt-2 text-lg md:text-2xl">{description}<br />
-        <Link to="blog/what-is-tctp"
+        <Link to="/about"
           className="underline text-green-light bg-blue-dark text-base md:text-xl tracking-wider rounded-md p-1
           hover:bg-green-light hover:text-blue-dark transition-all duration-200">Learn more...</Link>
       </span>
     </div>
-    <Link to="blog/what-is-tctp" className="my-1 md:my-0">
+    <Link to="/about" className="my-1 md:my-0">
       <img src={image} alt="assortment of electronic components" className="rounded-md opacity-70 w-5/6 md:w-full
       shadow-md hover:opacity-90 transition-all duration-200" />
     </Link>
