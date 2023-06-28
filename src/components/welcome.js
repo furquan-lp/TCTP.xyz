7f9eee9cde9b9440ad60566608ab96f3f99981be
Syed F Ahmad <furquan@protonmail.com>
2023-06-29 00:38:15 +0530

    Fixed the styling and increased margins

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -2,17 +2,16 @@ import React from 'react';
 import { Link } from 'gatsby';
 
 const Welcome = ({ description, image }) =>
-  <div className="flex justify-between mt-4 md:mt-12 flex-wrap md:flex-nowrap">
+  <div className="flex mt-4 md:mt-12 flex-wrap md:flex-nowrap">
     <div className="flex flex-col">
-      <span className="md:underline text-4xl md:text-6xl tracking-wider">Welcome!</span>
-      <span className="tracking-wide mt-2 text-lg md:text-2xl">{description}<br />
-        <Link to="/about"
-          className="underline text-green-light bg-blue-dark text-base md:text-xl tracking-wider rounded-md p-1
-          hover:bg-green-light hover:text-blue-dark transition-all duration-200">Learn more...</Link>
-      </span>
+      <span className="md:underline mb-4 text-4xl md:text-6xl tracking-wider">Welcome!</span>
+      <span className="tracking-wide my-2 text-lg md:text-2xl">{description}</span>
+      <Link to="/about"
+        className="underline text-green-light bg-blue-dark text-base md:text-xl tracking-wider rounded-md p-1
+        mr-auto hover:bg-green-light hover:text-blue-dark transition-all duration-200">Learn more...</Link>
     </div>
     <Link to="/about" className="my-1 md:my-0">
-      <img src={image} alt="assortment of electronic components" className="rounded-md opacity-70 w-5/6 md:w-full
+      <img src={image} alt="assortment of electronic components" className="rounded-md opacity-70 w-5/6 md:w-full h-full
       shadow-md hover:opacity-90 transition-all duration-200" />
     </Link>
   </div>;
